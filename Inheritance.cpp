0de631b9e64dc8b25f5b81186f6a6446ea2655a8
kalycaokta-ux <kalycaokta@gmail.com>
2026-05-12 18:29:54 +0700

    menambahkan class pelajar

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -4,3 +4,35 @@
 using namespace std;
 
 class orang {
+public:
+    string nama;
+
+    orang(string pNama) :
+        nama(pNama) {
+        cout << "orang dibuat\n" << endl;
+    }
+    ~orang() {
+        cout << "orang dihapus\n" << endl;
+    }
+
+    int jumlah(int a, int b) {
+        return a + b;
+    }
+
+};
+
+class pelajar : public orang {
+public:
+    string sekolah;
+
+    pelajar(string pNama, string pSekolah) : orang(pNama), sekolah(pSekolah) {
+        cout << "pelajar dibuat\n" << endl;
+    }
+    ~pelajar() {
+        cout << "pelajar dihapus\n" << endl;
+    }
+    string perkenalan() {
+        return "Hallo, nama saya " + nama + " dari sekolah " + sekolah + "\n
+\n";
+    }
+};
